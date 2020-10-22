47efc6c199274ff94da5cef3141afc4facae8bf4
fragmentedbin <muhammadmaududy4@gmail.com>
2020-10-22 19:00:53 +0700

    GVM Project

---
+++ public/js/style.js
@@ -0,0 +1,1 @@
+document.getElementById('hacim').innerHTML = "JAVA SUCCESS";
---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>GVM</title>
+
+        <!-- MDBootstrap Datatables  -->
+        <link href="{{ asset ('css/addons/datatables.min.css')}}" rel="stylesheet">
+        <!-- MDB icon -->
+        <link rel="icon" href="{{ asset ('img/icon.png')}}" type="image/x-icon">
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+        <!-- Google Fonts Roboto -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <!-- Your custom styles (optional) -->
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    </head>
+        <body>
+            <h1 class="btn btn-warning justifa">HELLO THERE!</h1>
+            <h1 id="hacim"></h1>
+        </body>
+    <!-- jQuery -->
+    <script type=" text/javascript" src="{{asset('js/jquery.min.js')}}">
+    </script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="{{asset('js/popper.min.js')}}"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
+    <!-- MDBootstrap Datatables  -->
+    <script type="text/javascript" src="{{asset('js/addons/datatables.min.js')}}"></script>
+    <!-- Your custom scripts (optional) -->
+    <script type="text/javascript" src="{{asset('js/style.js')}}"></script>
+
+    <link rel="stylesheet" type="text/css" href="{{asset('css/addons/datatables.min.css')}}"/>
+
+    <script type="text/javascript" src="{{asset('js/addons/pdfmake.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/addons/vfs_fonts.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/addons/datatables.min.js')}}"></script>
+</html>
