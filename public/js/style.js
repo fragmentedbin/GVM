d88123202ffeb1442a0ddcc53064ac3bcc540e77
fragmentedbin <muhammadmaududy4@gmail.com>
2020-10-22 22:57:10 +0700

    update

--- public/js/style.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('hacim').innerHTML = "JAVA SUCCESS";
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,8 +21,52 @@
         <link rel="stylesheet" href="{{asset('css/style.css')}}">
     </head>
         <body>
-            <h1 class="btn btn-warning justifa">HELLO THERE!</h1>
-            <h1 id="hacim"></h1>
+            <div class="top-container-c">
+                <div class="top-wrapper-c">
+                    <nav class="navbar top">
+                        <span class="navbar-brand"><img src="{{asset('img/logo-navbar.jpg')}}" alt=""></span>
+                        <div class="vibes">
+                            <h1>GROW VIBES</h1>
+                            <p>Music</p>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div class="bottom-container-c">
+                <div class="bottom-wrapper-c">
+                    <nav class="navbar bottom navbar-expand-lg navbar-dark bg-dark">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                          <ul class="navbar-nav">
+                            <li class="nav-item active">
+                              <a class="nav-link" href="#">All releases</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Submit Track</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Our Artist</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Partner Labels</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Profile Grow Vibes Music</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Our Team</a>
+                            </li>
+                            <li class="nav-item">
+                              <a class="nav-link" href="#">Contact With Us</a>
+                            </li>
+                          </ul>
+                        </div>
+                      </nav>
+                </div>
+            </div>
+            <center><h1>TEST</h1></center>
         </body>
     <!-- jQuery -->
     <script type=" text/javascript" src="{{asset('js/jquery.min.js')}}">
