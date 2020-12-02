d96b7589a83864965ac363dfb67182337d45fd2a
fragmentedbin <muhammadmaududy4@gmail.com>
2020-12-02 14:27:21 +0700

    update

--- resources/views/layout/templateMaster.blade.php
+++ resources/views/layout/templateMaster.blade.php
@@ -20,7 +20,8 @@ $featureAccess = 0;
     <!-- Google Fonts Roboto -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}"> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <!-- Your custom styles (optional) -->
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
@@ -31,19 +32,23 @@ $featureAccess = 0;
     @include('layout._footer')
 </body>
 <!-- jQuery -->
-<script type=" text/javascript" src="{{asset('js/jquery.min.js')}} defer"></script>
+{{-- <script type=" text/javascript" src="{{asset('js/jquery.min.js')}} defer"></script> --}}
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <!-- Bootstrap tooltips -->
-<script type="text/javascript" src="{{asset('js/popper.min.js')}} defer"></script>
+{{-- <script type="text/javascript" src="{{asset('js/popper.min.js')}} defer"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <!-- Bootstrap core JavaScript -->
-<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}} defer"></script>
+{{-- <script type="text/javascript" src="{{asset('js/bootstrap.min.js')}} defer"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
 <!-- MDBootstrap Datatables  -->
-<script type="text/javascript" src="{{asset('js/addons/datatables.min.js')}} defer"></script>
+{{-- <script type="text/javascript" src="{{asset('js/addons/datatables.min.js')}} defer"></script> --}}
 <!-- Your custom scripts (optional) -->
 <script type="text/javascript" src="{{asset('js/style.js')}} defer"></script>
 
-<script type="text/javascript" src="{{asset('js/addons/pdfmake.min.js')}} defer"></script>
+{{-- UNUSED --}}
+{{-- <script type="text/javascript" src="{{asset('js/addons/pdfmake.min.js')}} defer"></script>
 <script type="text/javascript" src="{{asset('js/addons/vfs_fonts.js')}} defer"></script>
-<script type="text/javascript" src="{{asset('js/addons/datatables.min.js')}} defer"></script>
+<script type="text/javascript" src="{{asset('js/addons/datatables.min.js')}} defer"></script> --}}
 
 {{-- ADD-on --}}
 
