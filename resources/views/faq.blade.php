bac9236d15fc765e52c962f89807b017c96e8887
fragmentedbin <muhammadmaududy4@gmail.com>
2020-12-09 23:19:21 +0700

    update

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -1,18 +1,39 @@
 @extends('layout/templateMaster')
 @section('content')
 <div class="faq">
-    <div data-number="1" class="faq-qs d-flex p-2">
-        <div data-number-class="1" class="faq-span">Q: what 1?
-            <span class="show-btn" target="1"><i class="fas fa-chevron-down"></i></span>
-            <div data-number-class-sub="1" class="faq-answer" hidden>A: answer 1</div>
+    <div data-number="1" class="row d-flex align-items-center faq-qs">
+        <div class="col-11">
+            <div class="">
+                <span class="logo-qa"><i class="far fa-question-circle"></i></span>
+                <span class="faq-tittle-ans">what 1?</span>
+            </div>
         </div>
-    </div>
-
-    <div data-number="2" class="faq-qs d-flex p-2">
-        <div data-number-class="2" class="faq-span">Q: what 2?
-            <span class="show-btn" target="2"><i class="fas fa-chevron-down"></i></span>
-            <div data-number-class-sub="2" class="faq-answer" hidden>A: answer 2</div>
+        <div class="col-1">
+            <div class="">
+                <span class="show-btn" target="1"><i class="fas fa-chevron-down"></i></span>
+            </div>
+        </div>
+        <div data-number="1" class="row align-items-center">
+            <div class="col-11">
+                <div class="">
+                    <span class="logo-qa"><i class="far fa-check-circle"></i></span>
+                    <span class="faq-tittle-ans">answe 1!</span>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+{{-- <div data-number="1" class="faq-qs d-flex flex-row">
+    <div data-number-class="1" class="faq-span">
+        <div>
+            <span class="logo-qa"><i class="far fa-question-circle"></i></span><span class="faq-tittle-ans">what
+                1?</span>
+        </div>
+        <div class="mr-auto" style="display: inline !important;">
+            <span class="show-btn" target="1"><i class="fas fa-chevron-down"></i></span>
+        </div>
+        <div data-number-class-sub="1" class="faq-answer p-2"><span class="logo-qa">
+                <i class="far fa-check-circle"></i></span>answer 1</div>
+    </div>
+</div> --}}
 @endsection
--- resources/views/layout/_footer.blade.php
+++ resources/views/layout/_footer.blade.php
@@ -83,8 +83,6 @@
                                     src="{{asset('img/partner/redlipsmv.png')}}" alt=""></a></div>
                         <div class="grid-item"><a href="https://soundfren.id//"><img class="img-partner"
                                     src="{{asset('img/partner/soundfren.png')}}" alt=""></a></div>
-                        <div class="grid-item"><a href="https://www.ppluk.com/"><img class="img-partner"
-                                    src="{{asset('img/partner/ppl2.png')}}" alt=""></a></div>
                         <div class="grid-item"><a href="https://www.instagram.com/indonesiadancechart/"><img
                                     class="img-partner" src="{{asset('img/partner/indonesiadancingchart.png')}}"
                                     alt=""></a></div>
